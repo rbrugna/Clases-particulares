60ca5d88780c7e859ec8e4b8de5a40f65fe6db92
rbrugna <rbrugna@itba.edu.ar>
2026-02-26 12:31:44 -0300

    A

--- docs/js/firebase.js
+++ docs/js/firebase.js
@@ -1,4 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { getFunctions } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-functions.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCLMTSYxCBXS5P9zARGOrwyffAzDj7XU4E",
   authDomain: "clases-particulares-1ba72.firebaseapp.com",
@@ -8,3 +12,8 @@ const firebaseConfig = {
   appId: "1:1002503705019:web:756947d3ab65aa7517b49e",
   measurementId: "G-C8KN11E4X1"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const fx = getFunctions(app);
